c26a3f08215ab62e8809db9103a389d80900e89c
LITHESH MALEPATI <malepatilithu213@gmail.com>
2025-06-14 08:04:07 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const PORT = 3000;
-
+   
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
